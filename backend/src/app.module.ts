b2797bd55f9e6f06626bee0a59bff7ef0f25e7e8
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-07-08 19:14:10 +0200

    chore: nest g c controller/file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,11 @@ import getCommonConfig from './configs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app/app.service';
 import { AwsService } from '@/services/aws/aws.service';
+import { FileController } from '@/controllers/file/file.controller';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true, load: [getCommonConfig] })],
-  controllers: [AppController],
+  controllers: [AppController, FileController],
   providers: [AppService, AwsService],
 })
 export class AppModule {}
