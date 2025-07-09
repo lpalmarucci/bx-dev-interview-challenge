c5641350467c13ee75b3e8d89e8f76157749b840
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-07-09 22:54:02 +0200

    refactor: renamed routes file

--- frontend/src/app-init.tsx
+++ frontend/src/app-init.tsx
@@ -4,7 +4,7 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import {CssBaseline, StyledEngineProvider, ThemeProvider,} from "@mui/material";
 import theme from "./theme";
-import {browserRouter} from "./routes/constants.tsx";
+import {browserRouter} from "./routes/routes.tsx";
 import {RouterProvider} from "react-router";
 import {AuthProvider} from "./contexts/auth.context.tsx";
 
