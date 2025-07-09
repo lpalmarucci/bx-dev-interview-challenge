8a3ebd01f260a02a086b8687b88a6001759b12b6
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-07-09 14:48:04 +0200

    feat: nest g m modules/auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { AppService } from './services/app/app.service';
 import { AwsService } from '@/services/aws/aws.service';
 import { FileController } from '@/controllers/file/file.controller';
 import { FileService } from './services/file/file.service';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getCommonConfig] })],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [getCommonConfig] }), AuthModule],
   controllers: [AppController, FileController],
   providers: [AppService, AwsService, FileService],
 })
