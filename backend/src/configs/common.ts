dbf79d18f9edd7132c60a8977dee624cd1062f25
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-07-10 23:33:38 +0200

    fix(backend): empty string if env variables don't exist

--- backend/src/configs/common.ts
+++ backend/src/configs/common.ts
@@ -4,12 +4,12 @@ function getCommonConfig(): GlobalConfig {
   return {
     port: parseInt(process.env.APP_PORT ?? '3000', 10),
     jwt: {
-      secret: process.env.JWT_SECRET_KEY!,
-      expiresIn: process.env.JWT_EXPIRES_IN!,
+      secret: process.env.JWT_SECRET_KEY ?? '',
+      expiresIn: process.env.JWT_EXPIRES_IN ?? '',
     },
     aws: {
-      region: process.env.AWS_REGION!,
-      bucketName: process.env.S3_BUCKET_NAME!,
+      region: process.env.AWS_REGION ?? '',
+      bucketName: process.env.S3_BUCKET_NAME ?? '',
     },
   };
 }
