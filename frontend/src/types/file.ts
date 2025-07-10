7a6e221fe9be2469b2b158bb3b0a68dafab9cf25
Luca Palmarucci <lpalmarucci@tierratelematics.com>
2025-07-10 11:14:35 +0200

    refactor(frontend): rename types and utils files

--- frontend/src/types/file.ts
+++
@@ -1,4 +0,0 @@
-export type UploadFileResponseDto = {
-  url: string;
-  name: string;
-}
---
+++ frontend/src/types/file.types.ts
@@ -0,0 +1,6 @@
+export type FileResponseDto = {
+  url: string;
+  name: string;
+  size: number;
+  lastModified: Date;
+}
